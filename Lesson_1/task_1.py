848915bbaba396ab5b4d4f57b5fb72047ea91329
Anna Arkhipova <arkhipova-1997@yandex.ru>
2020-11-19 21:07:41 +0300

    lesson_1 / task_1

---
+++ Lesson_1/task_1.py
@@ -0,0 +1,10 @@
+# 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+# https://drive.google.com/file/d/1ydDhgRFY0F_4WXiqLSupQQdnR_XbU7Fo/view?usp=sharing
+
+print("Введи три числа")
+a = int(input("Первое - "))
+b = int(input("Второе - "))
+c = int(input("Третье - "))
+d = a + b + c
+f = a * b * c
+print(f"{d=}, {f=}")

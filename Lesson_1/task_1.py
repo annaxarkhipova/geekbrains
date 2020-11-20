e8f0e291c68d59016de066710e59c1dc86c7bbaa
Anna Arkhipova <arkhipova-1997@yandex.ru>
2020-11-20 18:20:33 +0300

    Lesson_1 / task_1: correction

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -1,13 +1,13 @@
 # 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 # https://drive.google.com/file/d/1ydDhgRFY0F_4WXiqLSupQQdnR_XbU7Fo/view?usp=sharing
 
-print("Введи три целых числа")
-a = int(input("Первое - "))
-b = int(input("Второе - "))
-c = int(input("Третье - "))
+print("Введи трехзначное число")
+a = int(input("Первая цифра - "))
+b = int(input("Вторая цифра - "))
+c = int(input("Третья цифра - "))
 if a < 0 or b < 0 or c < 0:
     print("Решений нет")
 else:
     d = a + b + c
     f = a * b * c
-    print(f"{d=}, {f=}")
+    print(f"Сумма цифр {d=}, произведение - {f=}")

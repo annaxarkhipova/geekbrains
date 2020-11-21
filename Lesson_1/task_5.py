188f0a6a6c820a73b7556d34705e95b7ad159cf2
Anna Arkhipova <arkhipova-1997@yandex.ru>
2020-11-21 16:02:47 +0300

    Lesson_1 / task_5 : correction

--- Lesson_1/task_5.py
+++ Lesson_1/task_5.py
@@ -5,8 +5,11 @@
 print("Введи любые две буквы")  # строчные буквы от a-z
 first_letter = ord(input("Первая "))
 second_letter = ord(input("Вторая "))
-first_letter_place = abs(ord("a") - first_letter) + 1
-second_letter_place = abs(ord("a") - second_letter) + 1
-print(f"{first_letter_place= }, {second_letter_place= }")
-amount_symbols_between_letters = abs(first_letter_place - second_letter_place) - 1
-print(f"{amount_symbols_between_letters= }")
+if first_letter != ord(chr(first_letter).lower()) or second_letter != ord(chr(second_letter).lower()):
+    print("error")
+else:
+    first_letter_place = abs(ord("a") - first_letter) + 1
+    second_letter_place = abs(ord("a") - second_letter) + 1
+    print(f"{first_letter_place= }, {second_letter_place= }")
+    amount_symbols_between_letters = abs(first_letter_place - second_letter_place) - 1
+    print(f"{amount_symbols_between_letters= }")

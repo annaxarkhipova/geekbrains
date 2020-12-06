ed7e518d72238ed5bfd9067a5fb758fe19af1a1b
Anna Arkhipova <arkhipova-1997@yandex.ru>
2020-12-06 13:38:48 +0300

    Lesson_3 / task_8: correcntion

--- Lesson_3/task_8.py
+++ Lesson_3/task_8.py
@@ -8,15 +8,20 @@ SIZE_line = 5
 SIZE_elem = 4
 
 
-s = 0
-for i in range(SIZE_line):
-    matrix = list(input(f"введи 3 цифры для  матрицы, 4ый элемент - сумма введенных тобой чисел: "))
-    if len(matrix) != SIZE_elem - 1:
-        print("Введи 3 цифры")
-        break
-    for a in matrix:
-        s += int(a)
-    print([int(f) for f in matrix] + [s], sep="\n")
+def foo():
     s = 0
+    matrix_output = []
+
+    for i in range(SIZE_line):
+        matrix_input = list(input(f"введи 3 цифры для  матрицы, 4ый элемент - сумма введенных тобой чисел: "))
+        if len(matrix_input) != SIZE_elem - 1:
+            print("Введи 3 цифры")
+            break
+        for a in matrix_input:
+            s += int(a)
+        matrix_output.insert(i, [int(f) for f in matrix_input] + [s])
+        s = 0
+    return matrix_output
 
 
+print(*foo(), sep="\n")

155f62f8476eeb7ac4fa1b5741f440c20c9247e2
Anna Arkhipova <arkhipova-1997@yandex.ru>
2020-12-06 18:53:13 +0300

    Lesson_3 / task_9: did my best

---
+++ Lesson_3/task_9.py
@@ -0,0 +1,45 @@
+# 9. Найти максимальный элемент среди минимальных элементов столбцов матрицы.
+import random
+
+SIZE_M = 4
+SIZE_N = 6
+MIN_ITEM = 0
+MAX_ITEM = 100
+matrix = [[random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE_N)] for _ in range(SIZE_M)]
+print(*matrix, sep="\n")
+
+
+minimal_digits = []
+for i in range(SIZE_N):
+    matrix_columns = [line[i] for line in matrix]
+    minimum = [matrix_columns[0]]
+    for symbol in matrix_columns:
+        if symbol < minimum[-1]:
+            minimum = [symbol]
+    minimal_digits.extend(minimum)
+
+maximum = [minimal_digits[0]]
+for symbol in minimal_digits:
+    if symbol > minimal_digits[-1]:
+        maximum = [symbol]
+print(*maximum)
+
+
+
+
+
+
+# for i in range(SIZE_N):
+#     matrix_columns = [line[i] for line in matrix]
+#     minimum = [matrix_columns[0]]  # надо получить список [x,x,x], а не [x] [x] [x]
+#
+#     for symbol in matrix_columns:
+#         if symbol < minimum[-1]:
+#             minimum = [symbol]
+#
+#     maximum = [minimum[0]]
+#
+#     for max_symbol in minimum:
+#         if max_symbol > maximum[-1]:
+#             maximum = [max_symbol]
+#     print(maximum)

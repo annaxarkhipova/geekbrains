013a310c40ddf36c70d6643e9e28f3882d74a25b
Anna Arkhipova <arkhipova-1997@yandex.ru>
2020-12-07 20:11:58 +0300

    Lesson_3 / task_9

--- Lesson_3/task_9.py
+++ Lesson_3/task_9.py
@@ -8,18 +8,18 @@ MAX_ITEM = 100
 matrix = [[random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE_N)] for _ in range(SIZE_M)]
 print(*matrix, sep="\n")
 
-
 minimal_digits = []
 for i in range(SIZE_N):
     matrix_columns = [line[i] for line in matrix]
-    minimum = [matrix_columns[0]]
+    minimum = matrix_columns[0]
     for symbol in matrix_columns:
-        if symbol < minimum[-1]:
-            minimum = [symbol]
-    minimal_digits.extend(minimum)
+        if symbol < minimum:
+            minimum = symbol
+    minimal_digits.append(minimum)
 
-maximum = [minimal_digits[0]]
+maximum = minimal_digits[0]
 for symbol in minimal_digits:
-    if symbol > minimal_digits[-1]:
-        maximum = [symbol]
-print(*maximum)
+    if symbol > maximum:
+        maximum = symbol
+print(maximum)
+

36d351e60299192fda3dacf670ce63f5805c19c5
Anna Arkhipova <arkhipova-1997@yandex.ru>
2020-12-13 19:21:02 +0300

    Lesson_4 / task_1

--- Lesson_4/task_1.py
+++ Lesson_4/task_1.py
@@ -13,18 +13,18 @@ import cProfile
 from Lesson_2.task_4 import program
 
 # var 1 (imported)
-# print(timeit.timeit("program(900)", number=100, globals=globals()))
-# print(cProfile.run("program(900)"))
-# 0.25896524
-#          903 function calls (4 primitive calls) in 0.003 seconds
+print(timeit.timeit("program(900)", number=100, globals=globals()))
+print(cProfile.run("program(900)"))
+# 0.201378014
+#           903 function calls (4 primitive calls) in 0.002 seconds
 #
-#    Ordered by: standard name
+# Ordered by: standard name
 #
-#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#         1    0.000    0.000    0.002    0.002 <string>:1(<module>)
-#     900/1    0.002    0.000    0.002    0.002 task_4.py:6(program)
-#         1    0.000    0.000    0.003    0.003 {built-in method builtins.exec}
-#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#      1    0.000    0.000    0.002    0.002 <string>:1(<module>)
+#  900/1    0.002    0.000    0.002    0.002 task_1.py:52(program3)
+#      1    0.000    0.000    0.002    0.002 {built-in method builtins.exec}
+#      1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
 
 # var 2
@@ -48,3 +48,26 @@ print(cProfile.run("program2(900)"))
 #         1    0.000    0.000    0.002    0.002 {built-in method builtins.exec}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
+# var 3
+def program3(a):
+    element = 0
+    for k in range(1, a - 1):
+        for p in range(2, a):
+            element = -k / p
+        else:
+            element += element / p
+    return element
+
+
+print(timeit.timeit("program3(900)", number=100, globals=globals()))
+print(cProfile.run("program3(900)"))
+# 8.846706838
+#          4 function calls in 0.095 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.095    0.095 <string>:1(<module>)
+#         1    0.095    0.095    0.095    0.095 task_1.py:52(program3)
+#         1    0.000    0.000    0.095    0.095 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}

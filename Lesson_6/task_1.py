baa47b9b3a096729090e3b436a502cbf5a8e3bf5
Anna Arkhipova <arkhipova-1997@yandex.ru>
2021-01-12 20:50:37 +0300

    fix

--- Lesson_6/task_1.py
+++ Lesson_6/task_1.py
@@ -7,6 +7,7 @@
 # результаты анализа (количество занятой памяти в вашей среде разработки) вставить в виде комментариев в файл с кодом.
 # Не забудьте указать версию и разрядность вашей ОС и интерпретатора Python;
 # написать общий вывод: какой из трёх вариантов лучше и почему.
+
 # OS & Python 3.8.1 - 64-bit
 import sys
 from collections import deque, Counter
@@ -59,9 +60,9 @@ print(memory_count())  # 5648
 MIN_ITEM = 2
 MAX_ITEM = 9
 multiples = {}  # Инициализация словаря c помощью литерала
-memory_count(multiples)
+memory_count(multiples, MAX_ITEM, MIN_ITEM)
 
-for p in range(2, 9 + 1):
+for p in range(MIN_ITEM, MAX_ITEM + 1):
     multiples[p] = [n for n in range(2, 99 + 1) if n % p == 0]
     memory_count(p)
     memory_count(multiples[p])
@@ -69,5 +70,5 @@ for p in range(2, 9 + 1):
 for key, val in multiples.items():
     memory_count(print(f"{key}", ", ".join(str(v) for v in val), sep=" -> "))
 
-print(memory_count())  # 2520
+print(memory_count())  # 2576
 
